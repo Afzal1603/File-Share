52f595cce8fd20960a6587d332ccb789be222513
Mohd Afzal Ansari <159166024+Afzal1603@users.noreply.github.com>
2025-02-11 22:14:14 +0530

    Update api.js

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,10 +1,10 @@
 import axios from "axios";
 
-const API_URL = import.meta.env.PROD ? "/" : "http://localhost:5000/api";
+const API_URL = import.meta.env.PROD ? "https://file-share-8i1y.onrender.com" : "http://localhost:5000";
 
 export const uploadFile = async (data) => {
   try {
-    const response = await axios.post(`${API_URL}/upload`, data);
+    const response = await axios.post(`${API_URL}/api/upload`, data);
     return response.data;
   } catch (error) {
     console.error(error);
