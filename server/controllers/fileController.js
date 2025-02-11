17a47149927871a8b74df37256fe431048f31d87
Mohd Afzal Ansari <159166024+Afzal1603@users.noreply.github.com>
2025-02-11 21:42:56 +0530

    Update fileController.js

--- server/controllers/fileController.js
+++ server/controllers/fileController.js
@@ -9,7 +9,7 @@ const fileUpload = async (req, res) => {
     const file = await File.create(fileObj);
     return res
       .status(200)
-      .json({ path: `http://localhost:5000/api/files/${file._id}` });
+      .json({ path:process.env.NODE_ENV==="production"?`https://file-share-8i1y.onrender.com`:`http://localhost:5000/api/files/${file._id}` });
   } catch (error) {
     console.error(error);
     return res.status(500).json({ message: "Error uploading file" });
